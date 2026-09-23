f240b23fecb8ae37625f9aafdfa3118d29bcfe6f
georgeissa <gissa1980@gmail.com>
2026-09-23 20:19:37 +0300

    Add my new file to the repository

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
 import Image from "next/image";
+import styles from '@/app/ui/home.module.css';
+import InvoiceStatus from "./ui/invoices/status";
+import { lusitana } from '@/app/ui/fonts';
 
 export default function Home() {
   return (
@@ -14,13 +17,17 @@ export default function Home() {
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the{" "}
+            To get started, chande the{" "}
             <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
               page.tsx
             </code>{" "}
             file.
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+          <div className={styles.shape} />
+         <InvoiceStatus status="pending"/>
+         <InvoiceStatus status="paid"/>
+         
+          <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             Looking for a starting point or more instructions? Head over to{" "}
             <a
               href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
